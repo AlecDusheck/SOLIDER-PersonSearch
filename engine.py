828a6c5aac330aa2ec9f61ec456e47391780da02
Alec Dusheck <alec@simplyalec.com>
2024-10-31 15:47:44 -0500

    Update engine.py

--- engine.py
+++ engine.py
@@ -41,13 +41,15 @@ def train_one_epoch(cfg, model, optimizer, data_loader, device, epoch, tfboard=N
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = reduce_dict(loss_dict)
+        
+        # Replace NaN losses with zero
+        for k in loss_dict_reduced:
+            if torch.isnan(loss_dict_reduced[k]):
+                print(f"{k} is NaN, setting it to zero")
+                loss_dict_reduced[k] = torch.tensor(0.0, device=loss_dict_reduced[k].device)
+        
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
         loss_value = losses_reduced.item()
-
-        if not math.isfinite(loss_value):
-            print(f"Loss is {loss_value}, stopping training")
-            print(loss_dict_reduced)
-            sys.exit(1)
 
         optimizer.zero_grad()
         losses.backward()
